629cf3d56984f93ba9f7ff6ad71e4fc460e30b2d
stevenkim18 <11882@naver.com>
2021-09-07 14:40:02 +0900

    feat: NetworkError 타입 구현

--- OpenMarket/Models/Network/NetworkManager.swift
+++ OpenMarket/Models/Network/NetworkManager.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+typealias ResultHandler = (Result<Data, NetworkError>) -> Void
+
 struct NetworkManager {
     let session: URLSession
     
@@ -14,26 +16,33 @@ struct NetworkManager {
         self.session = session
     }
     
-    func request(_ endPoint: EndPoint, completion: @escaping (Result<Data, Error>) -> Void) {
+    func request(_ endPoint: EndPoint, completion: @escaping ResultHandler) {
         guard let url = URL(string: endPoint.url) else {
-            return completion(.failure(fatalError()))
+            return completion(.failure(.invalidURL))
         }
+        
         let urlRequest = generateRequest(with: url, endPoint)
-        _ = session.dataTask(with: urlRequest) { data, response, error in
+        
+        session.dataTask(with: urlRequest) { data, response, error in
             guard error == nil else {
-                return completion(.failure(fatalError()))
+                return completion(.failure(.requestError))
             }
+            
             guard let response = response as? HTTPURLResponse else {
-                return completion(.failure(fatalError()))
+                return completion(.failure(.invalidURLHTTPResponse))
             }
+            
             guard (200...399).contains(response.statusCode) else {
-                return completion(.failure(fatalError()))
+                return completion(.failure(.invalidHTTPStatusCode(response.statusCode)))
             }
+            
             guard let data = data else {
-                return completion(.failure(fatalError()))
+                return completion(.failure(.invalidData))
             }
+            
             completion(.success(data))
-        }
+            
+        }.resume()
     }
     
     private func generateRequest(with url: URL, _ endPoint: EndPoint) -> URLRequest {
