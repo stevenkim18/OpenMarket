83ce1b5d3aa4d902bbcd5823c45b6d407bcd3681
stevenkim18 <11882@naver.com>
2021-09-07 10:35:30 +0900

    feat: HttpMethod, EndPoint 타입 구현, 서버 api 주소 상수 분리

---
+++ OpenMarket/Constants/ServerAPI.swift
@@ -0,0 +1,14 @@
+//
+//  ServerAPI.swift
+//  OpenMarket
+//
+//  Created by steven on 9/7/21.
+//
+
+import Foundation
+
+enum ServerAPI {
+    static let baseURL = "https://camp-open-market-2.herokuapp.com/"
+    static let listPath = "items/"
+    static let itemPath = "item/"
+}
---
+++ OpenMarket/Models/Network/EndPoint.swift
@@ -0,0 +1,46 @@
+//
+//  EndPoint.swift
+//  OpenMarket
+//
+//  Created by steven on 9/7/21.
+//
+
+import Foundation
+
+enum EndPoint {
+    case readList(Int)
+    case readItem(Int)
+    case createItem
+    case updateItem(Int)
+    case deleteItem(Int)
+    
+    var url: String {
+        switch self {
+        case .readList(let page):
+            return ServerAPI.baseURL + ServerAPI.listPath + "\(page)"
+        case .readItem(let id):
+            return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
+        case .createItem:
+            return ServerAPI.baseURL + ServerAPI.itemPath
+        case .updateItem(let id):
+            return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
+        case .deleteItem(let id):
+            return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
+        }
+    }
+    
+    var httpMethod: HttpMethod {
+        switch self {
+        case .readList:
+            return .get
+        case .readItem:
+            return .get
+        case .createItem:
+            return .post
+        case .updateItem:
+            return .patch
+        case .deleteItem:
+            return .delete
+        }
+    }
+}
