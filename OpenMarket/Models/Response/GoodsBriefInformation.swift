0dc832d9fa1674fa33af124cb390f3817f636068
stevenkim18 <11882@naver.com>
2021-09-08 18:24:14 +0900

    feat: 상품 목록 화면 모델 구현

---
+++ OpenMarket/Models/Response/GoodsBriefInformation.swift
@@ -0,0 +1,19 @@
+//
+//  GoodsBriefInformation.swift
+//  OpenMarket
+//
+//  Created by steven on 9/7/21.
+//
+
+import Foundation
+
+struct GoodsBriefInfomation: ResponseGoodsInformation {
+    var id: Int
+    var title: String
+    var price: Int
+    var currency: String
+    var stock: Int
+    var discountedPrice: Int?
+    var thumbnails: [String]
+    var registrationData: Double
+}
