84ce254ca709917899de4fcf16b38d8cf0e2c08c
stevenkim18 <11882@naver.com>
2021-09-15 18:00:14 +0900

    refactor: Endpoint 중복되는 케이스 제거

--- OpenMarket/Models/Network/EndPoint.swift
+++ OpenMarket/Models/Network/EndPoint.swift
@@ -14,27 +14,20 @@ enum EndPoint {
     case updateItem(Int)
     case deleteItem(Int)
     
-    // TODO: 중복되는 케이스 합치기
     var url: String {
         switch self {
         case .readList(let page):
             return ServerAPI.baseURL + ServerAPI.listPath + "\(page)"
-        case .readItem(let id):
+        case .readItem(let id), .updateItem(let id), .deleteItem(let id):
             return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
         case .createItem:
             return ServerAPI.baseURL + ServerAPI.itemPath
-        case .updateItem(let id):
-            return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
-        case .deleteItem(let id):
-            return ServerAPI.baseURL + ServerAPI.itemPath + "\(id)"
         }
     }
     
     var httpMethod: HttpMethod {
         switch self {
-        case .readList:
-            return .get
-        case .readItem:
+        case .readList, .readItem
             return .get
         case .createItem:
             return .post
